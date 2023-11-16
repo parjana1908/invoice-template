b62e91cf200c30d58b9d5c5548d66e1a3fec310f
parjana1908 <145995671+parjana1908@users.noreply.github.com>
2023-11-16 20:20:49 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Template from './Template';
+
+function App() {
+  return (
+    <>
+     <Template />
+  
+    </>
+  );
+}
+
+export default App;
